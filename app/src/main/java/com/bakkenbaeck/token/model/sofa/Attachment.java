8c4148d51e06137a7d85c2493ac115665a4148e5
Martin Tøften <martin@bakkenbaeck.no>
2017-01-23 13:14:53 +0100

    Added the objects Control and Attachment to the model

---
+++ app/src/main/java/com/bakkenbaeck/token/model/sofa/Attachment.java
@@ -0,0 +1,14 @@
+package com.bakkenbaeck.token.model.sofa;
+
+public class Attachment {
+    private String type;
+    private String url;
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+}
