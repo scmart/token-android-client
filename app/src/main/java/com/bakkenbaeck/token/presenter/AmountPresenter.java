94e2c32b384fa0f7570fb04aa865230398d1ef9a
Martin Tøften <martin@bakkenbaeck.no>
2017-01-30 14:22:31 +0100

    Added default text to the value fields

--- app/src/main/java/com/bakkenbaeck/token/presenter/AmountPresenter.java
+++ app/src/main/java/com/bakkenbaeck/token/presenter/AmountPresenter.java
@@ -2,6 +2,8 @@ package com.bakkenbaeck.token.presenter;
 
 import com.bakkenbaeck.token.view.activity.AmountActivity;
 
+import java.util.Locale;
+
 public class AmountPresenter implements Presenter<AmountActivity> {
 
     private AmountActivity activity;
@@ -9,6 +11,12 @@ public class AmountPresenter implements Presenter<AmountActivity> {
     @Override
     public void onViewAttached(AmountActivity view) {
         this.activity = view;
+        initView();
+    }
+
+    private void initView() {
+        this.activity.getBinding().usdValue.setText(String.format(Locale.getDefault(), "%s %d", "$", 0));
+        this.activity.getBinding().ethValue.setText(String.format(Locale.getDefault(), "%d %s", 0, "ETH"));
     }
 
     @Override
