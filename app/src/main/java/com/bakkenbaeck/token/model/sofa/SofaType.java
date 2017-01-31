2f4c76f83bd41a25b8f80a15666e4c63fd8a832e
Martin Tøften <martin@bakkenbaeck.no>
2017-01-31 13:21:46 +0100

    Added two SOFA message types

--- app/src/main/java/com/bakkenbaeck/token/model/sofa/SofaType.java
+++ app/src/main/java/com/bakkenbaeck/token/model/sofa/SofaType.java
@@ -11,6 +11,8 @@ public class SofaType {
             PAYMENT_REQUEST,
             COMMAND_REQUEST,
             PAYMENT,
+            INIT,
+            INIT_REQUEST,
     })
     public @interface Type {}
     public static final int UNKNOWN = -1;
@@ -18,6 +20,8 @@ public class SofaType {
     public static final int PAYMENT_REQUEST = 1;
     public static final int COMMAND_REQUEST = 2;
     public static final int PAYMENT = 3;
+    public static final int INIT = 4;
+    public static final int INIT_REQUEST = 5;
 
     public static final String LOCAL_ONLY_PAYLOAD = "custom_local_only_payload";
     public static final String WEB_VIEW = "webview:";
@@ -26,6 +30,8 @@ public class SofaType {
     private static final String command_request = "SOFA::Command:";
     private static final String payment_request = "SOFA::PaymentRequest:";
     private static final String payment = "SOFA::Payment:";
+    private static final String init = "SOFA::Init:";
+    private static final String init_request = "SOFA::InitRequest:";
 
     public static String createHeader(final @SofaType.Type int type) {
         switch (type) {
@@ -33,6 +39,8 @@ public class SofaType {
             case PAYMENT_REQUEST: return payment_request;
             case COMMAND_REQUEST: return command_request;
             case PAYMENT: return payment;
+            case INIT: return init;
+            case INIT_REQUEST: return init_request;
             default: return null;
         }
     }
@@ -43,6 +51,8 @@ public class SofaType {
             case payment_request: return PAYMENT_REQUEST;
             case command_request: return COMMAND_REQUEST;
             case payment: return PAYMENT;
+            case init: return INIT;
+            case init_request: return INIT_REQUEST;
             default: return UNKNOWN;
         }
     }
