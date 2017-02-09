1f5170feabf4cc037b0c668ff1c7b0d85cd4a13f
Martin Tøften <martin@bakkenbaeck.no>
2017-02-09 19:23:24 +0100

    Added content to ChooseActivityActivity

--- app/src/main/java/com/bakkenbaeck/token/presenter/ChooseContactPresenter.java
+++ app/src/main/java/com/bakkenbaeck/token/presenter/ChooseContactPresenter.java
@@ -1,27 +1,53 @@
 package com.bakkenbaeck.token.presenter;
 
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.bakkenbaeck.token.R;
 import com.bakkenbaeck.token.crypto.util.TypeConverter;
+import com.bakkenbaeck.token.model.local.User;
 import com.bakkenbaeck.token.util.EthUtil;
 import com.bakkenbaeck.token.util.ViewTypePayment;
 import com.bakkenbaeck.token.view.BaseApplication;
+import com.bakkenbaeck.token.view.activity.ChatActivity;
 import com.bakkenbaeck.token.view.activity.ChooseContactsActivity;
+import com.bakkenbaeck.token.view.adapter.ContactsAdapter;
+import com.bakkenbaeck.token.view.custom.HorizontalLineDivider;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class ChooseContactPresenter implements Presenter<ChooseContactsActivity> {
 
+    private @ViewTypePayment.ViewType int viewType;
     private ChooseContactsActivity activity;
     private String encodedEthAmount;
-    private @ViewTypePayment.ViewType int viewType;
+    private ContactsAdapter adapter;
+    private User recipientUser;
+    private String localCurrency;
+    private boolean firstTimeAttaching = true;
 
     @Override
     public void onViewAttached(final ChooseContactsActivity view) {
         this.activity = view;
 
+        if (firstTimeAttaching) {
+            firstTimeAttaching = false;
+            initLongLivingObjects();
+        }
+
         getIntentData();
-        initToolbar();
+        getLocalCurrency();
+        initView();
+    }
+
+    private void initLongLivingObjects() {
+        this.adapter = new ContactsAdapter()
+                .loadAllStoredContacts()
+                .setOnItemClickListener(this::handleItemClicked);
     }
 
     @SuppressWarnings("WrongConstant")
@@ -30,17 +56,80 @@ public class ChooseContactPresenter implements Presenter<ChooseContactsActivity>
         this.viewType = this.activity.getIntent().getIntExtra(ChooseContactsActivity.VIEW_TYPE, ViewTypePayment.TYPE_SEND);
     }
 
-    private void initToolbar() {
+    private void getLocalCurrency() {
         final BigInteger weiAmount = TypeConverter.StringHexToBigInteger(this.encodedEthAmount);
         final BigDecimal ethAmount = EthUtil.weiToEth(weiAmount);
-        final String localCurrency = BaseApplication.get().getTokenManager().getBalanceManager().convertEthToLocalCurrencyString(ethAmount);
+        this.localCurrency = BaseApplication.get().getTokenManager().getBalanceManager().convertEthToLocalCurrencyString(ethAmount);
+    }
+
+    private void initView() {
+        initToolbar();
+        initRecyclerView();
+        updateEmptyState();
+        updateConfirmationBtnState();
+        setSendButtonText();
+
+        this.activity.getBinding().btnContinue.setOnClickListener(view -> handleSendClicked());
+    }
+
+    private void initToolbar() {
         final String paymentAction = this.viewType == ViewTypePayment.TYPE_SEND
                 ? this.activity.getString(R.string.send)
                 : this.activity.getString(R.string.request);
 
-        final String title = this.activity.getString(R.string.send_amount, paymentAction, localCurrency);
+        final String title = this.activity.getString(R.string.send_amount, paymentAction, this.localCurrency);
         this.activity.getBinding().title.setText(title);
         this.activity.getBinding().closeButton.setOnClickListener(view -> this.activity.finish());
+    }
+
+    private void initRecyclerView() {
+        final RecyclerView recyclerView = this.activity.getBinding().contacts;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.activity));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        final int dividerLeftPadding = activity.getResources().getDimensionPixelSize(R.dimen.avatar_size_small)
+                + activity.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+        final HorizontalLineDivider lineDivider =
+                new HorizontalLineDivider(activity.getResources().getColor(R.color.divider))
+                        .setLeftPadding(dividerLeftPadding);
+        recyclerView.addItemDecoration(lineDivider);
+        recyclerView.setAdapter(this.adapter);
+    }
+
+    private void updateEmptyState() {
+        final boolean showingEmptyState = this.activity.getBinding().emptyStateSwitcher.getCurrentView().getId() == this.activity.getBinding().emptyState.getId();
+        final boolean shouldShowEmptyState = this.activity.getBinding().contacts.getAdapter().getItemCount() == 0;
+
+        if (shouldShowEmptyState && !showingEmptyState) {
+            this.activity.getBinding().emptyStateSwitcher.showPrevious();
+        } else if (!shouldShowEmptyState && showingEmptyState) {
+            this.activity.getBinding().emptyStateSwitcher.showNext();
+        }
+    }
+
+    private void updateConfirmationBtnState() {
+        this.activity.getBinding().btnContinue.setEnabled(this.recipientUser != null);
+
+        final int color = this.recipientUser != null
+                ? ContextCompat.getColor(this.activity, R.color.colorPrimary)
+                : ContextCompat.getColor(this.activity, R.color.textColorSecondary);
+        this.activity.getBinding().btnContinue.setTextColor(color);
+    }
+
+    private void setSendButtonText() {
+        final String btnText = this.activity.getString(R.string.button_send_amount, this.localCurrency);
+        this.activity.getBinding().btnContinue.setText(btnText);
+    }
+
+    private void handleItemClicked(final User user) {
+        this.recipientUser = user;
+        this.activity.getBinding().recipientUser.setText(user.getUsername());
+        updateConfirmationBtnState();
+    }
+
+    private void handleSendClicked() {
+        final Intent intent = new Intent(activity, ChatActivity.class);
+        intent.putExtra(ChatActivity.EXTRA__REMOTE_USER, recipientUser);
+        this.activity.startActivity(intent);
     }
 
     @Override
