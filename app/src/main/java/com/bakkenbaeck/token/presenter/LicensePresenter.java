fcfd7e798c1b2f25ab1239705718ceaa50f3c29d
Martin Tøften <martin@bakkenbaeck.no>
2017-02-17 13:54:13 +0100

    Basic setup of the licence activity view

--- app/src/main/java/com/bakkenbaeck/token/presenter/LicensePresenter.java
+++ app/src/main/java/com/bakkenbaeck/token/presenter/LicensePresenter.java
@@ -1,20 +1,38 @@
 package com.bakkenbaeck.token.presenter;
 
+import android.view.View;
+
 import com.bakkenbaeck.token.model.local.Library;
 import com.bakkenbaeck.token.view.activity.LicenseActivity;
 
 public class LicensePresenter implements Presenter<LicenseActivity> {
 
     private LicenseActivity activity;
+    private Library library;
 
     @Override
     public void onViewAttached(LicenseActivity view) {
         this.activity = view;
         getIntentData();
+        initView();
+        initClickListeners();
     }
 
     private void getIntentData() {
-        final Library library = this.activity.getIntent().getParcelableExtra(LicenseActivity.LIBRARY);
+        this.library = this.activity.getIntent().getParcelableExtra(LicenseActivity.LIBRARY);
+    }
+
+    private void initView() {
+        this.activity.getBinding().title.setText(library.getName());
+        this.activity.getBinding().license.setText(library.getLicence());
+    }
+
+    private void initClickListeners() {
+        this.activity.getBinding().closeButton.setOnClickListener(this::handleCloseButtonClicked);
+    }
+
+    private void handleCloseButtonClicked(final View v) {
+        this.activity.finish();
     }
 
     @Override
