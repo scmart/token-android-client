3e4d6917023dd0c425cb2f4791a1b3a859558b05
Martin Tøften <martin@bakkenbaeck.no>
2017-02-17 11:26:41 +0100

    Added a basic view to about screen

--- app/src/main/java/com/bakkenbaeck/token/presenter/AboutPresenter.java
+++ app/src/main/java/com/bakkenbaeck/token/presenter/AboutPresenter.java
@@ -1,5 +1,7 @@
 package com.bakkenbaeck.token.presenter;
 
+import com.bakkenbaeck.token.BuildConfig;
+import com.bakkenbaeck.token.R;
 import com.bakkenbaeck.token.view.activity.AboutActivity;
 
 public class AboutPresenter implements Presenter<AboutActivity> {
@@ -9,6 +11,22 @@ public class AboutPresenter implements Presenter<AboutActivity> {
     @Override
     public void onViewAttached(AboutActivity view) {
         this.activity = view;
+        initView();
+    }
+
+    private void initView() {
+        setVersionName();
+        setContactInfo();
+    }
+
+    private void setVersionName() {
+        final String versionName = BuildConfig.VERSION_NAME;
+        this.activity.getBinding().version.setText(versionName);
+    }
+
+    private void setContactInfo() {
+        final String contactInfo = this.activity.getString(R.string.contact_information);
+        this.activity.getBinding().contactInfo.setText(contactInfo);
     }
 
     @Override
