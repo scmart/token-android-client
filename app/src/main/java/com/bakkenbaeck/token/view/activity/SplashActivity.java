eaa9bde4ae14558affaa93ac3c2e5808431ad273
csdodd <colin@bakkenbaeck.no>
2017-02-22 16:40:37 +0100

    Hold on the splash screen until the wallet is loaded

--- app/src/main/java/com/bakkenbaeck/token/view/activity/SplashActivity.java
+++ app/src/main/java/com/bakkenbaeck/token/view/activity/SplashActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bakkenbaeck.token.BuildConfig;
+import com.bakkenbaeck.token.crypto.HDWallet;
+import com.bakkenbaeck.token.view.BaseApplication;
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,6 +24,16 @@ public class SplashActivity extends AppCompatActivity {
             Fabric.with(this, new Crashlytics());
         }
 
+        BaseApplication
+                .get()
+                .getTokenManager()
+                .getWallet()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::gotoMainActivity);
+    }
+
+    private void gotoMainActivity(final HDWallet unused) {
         final Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
