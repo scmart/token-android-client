c50bb7d1fecdd57b036357de6bbe99a3d992ad2b
Martin Tøften <martin@bakkenbaeck.no>
2017-02-23 18:04:43 +0100

    Added a custom TextView with shadow

---
+++ app/src/main/java/com/bakkenbaeck/token/view/custom/ShadowTextView.java
@@ -0,0 +1,71 @@
+package com.bakkenbaeck.token.view.custom;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.bakkenbaeck.token.R;
+
+public class ShadowTextView extends CardView {
+
+    private boolean shadowEnabled;
+    private String text;
+    private int cornerRadius;
+
+    public ShadowTextView(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public ShadowTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        parseAttributeSet(context, attrs);
+        init();
+    }
+
+    private void parseAttributeSet(final Context context, final @Nullable AttributeSet attrs) {
+        final TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ShadowTextView, 0, 0);
+        this.shadowEnabled = a.getBoolean(R.styleable.ShadowTextView_shadow, true);
+        this.text = a.getString(R.styleable.ShadowTextView_text);
+        this.cornerRadius = a.getDimensionPixelSize(R.styleable.ShadowTextView_cornerRadius, 0);
+        a.recycle();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.view_shadow_text_view, this);
+        initView();
+    }
+
+    private void initView() {
+        setText(this.text);
+        setCornerRadius(this.cornerRadius);
+        setShadow();
+    }
+
+    private void setCornerRadius(final int cornerRadius) {
+        this.setRadius(cornerRadius);
+    }
+
+    public void setShadow() {
+        if (!this.shadowEnabled) {
+            disableShadow();
+        }
+    }
+
+    public void enableShadow() {
+        this.setCardElevation(4f);
+    }
+
+    public void disableShadow() {
+        this.setCardElevation(0f);
+    }
+
+    public void setText(final String s) {
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(s);
+    }
+}
