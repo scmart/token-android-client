06f188890f6519ef1772fd9df79778b94cd53302
Martin Tøften <martin@bakkenbaeck.no>
2017-03-13 12:45:20 +0100

    Added content to view

--- app/src/main/java/com/tokenbrowser/presenter/FullscreenImagePresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/FullscreenImagePresenter.java
@@ -1,14 +1,38 @@
 package com.tokenbrowser.presenter;
 
+import android.view.WindowManager;
+
+import com.bumptech.glide.Glide;
 import com.tokenbrowser.view.activity.FullscreenImageActivity;
+
+import java.io.File;
 
 public class FullscreenImagePresenter implements Presenter<FullscreenImageActivity> {
 
     private FullscreenImageActivity activity;
+    private String filename;
 
     @Override
     public void onViewAttached(FullscreenImageActivity view) {
         this.activity = view;
+        getIntentData();
+        hideStatusBar();
+        initView();
+    }
+
+    private void getIntentData() {
+        this.filename = this.activity.getIntent().getStringExtra(FullscreenImageActivity.FILENAME);
+    }
+
+    private void hideStatusBar() {
+        this.activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void initView() {
+        final File file = new File(this.activity.getFilesDir(), this.filename);
+        Glide.with(this.activity)
+                .load(file)
+                .into(this.activity.getBinding().image);
     }
 
     @Override
