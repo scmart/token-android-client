71c36854b7acdb624c6eafb13ef9f2dfac6c172c
csdodd <colin@bakkenbaeck.no>
2017-03-14 15:58:30 +0100

    Add offline snackbar.
    Create an OfflineViewBasePresenterActivity that an Activity can choose to extend; by doing so the Activity needs to provide
    a view in which to render any eventual offline snackbar. This allows the Activity tehmselves to have control over whether or
    not an offline snackbar should be rendered whilst keeping the offline logic in one centralised class.

---
+++ app/src/main/java/com/tokenbrowser/view/activity/OfflineViewBasePresenterActivity.java
@@ -0,0 +1,76 @@
+package com.tokenbrowser.view.activity;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tokenbrowser.presenter.Presenter;
+import com.tokenbrowser.token.R;
+import com.tokenbrowser.util.LogUtil;
+import com.tokenbrowser.view.BaseApplication;
+import com.tokenbrowser.view.custom.OfflineViewRenderer;
+
+import rx.Subscription;
+
+public abstract class OfflineViewBasePresenterActivity<P extends Presenter<V>, V extends OfflineViewRenderer> extends BasePresenterActivity<P, V> {
+
+    private Subscription connectionSubscription;
+    private Snackbar offlineSnackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        attachConnectionObserver();
+    }
+
+    private void attachConnectionObserver() {
+        this.connectionSubscription =
+                BaseApplication
+                .get()
+                .isConnectedSubject()
+                .subscribe(this::handleConnectionChange);
+    }
+
+    private void handleConnectionChange(final Boolean isConnected) {
+        if (isConnected && this.offlineSnackbar == null) {
+            return;
+        }
+
+        if (isConnected) {
+            this.offlineSnackbar.dismiss();
+        } else {
+            createSnackbar();
+            this.offlineSnackbar.show();
+        }
+    }
+
+    private void createSnackbar() {
+        try {
+            this.offlineSnackbar = Snackbar.make(
+                    getPresenterView().getOfflineViewContainer(),
+                    BaseApplication.get().getText(R.string.error__offline),
+                    Snackbar.LENGTH_INDEFINITE);
+
+            // Workaround to get white text on certain phones that override defaults
+            final View view = this.offlineSnackbar.getView();
+            final TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+            tv.setTextColor(Color.WHITE);
+        } catch (final NullPointerException ex) {
+            LogUtil.i(getClass(), "Attempt to render offline snackbar into null view.");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        this.connectionSubscription.unsubscribe();
+        super.onStop();
+    }
+}
