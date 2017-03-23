23c48ff45eb653b450ad794dc132d3e9c6b17d8c
csdodd <colin@bakkenbaeck.no>
2017-03-23 13:54:03 +0100

    Different layouts for payment requests

--- app/src/main/java/com/tokenbrowser/view/adapter/viewholder/PaymentRequestViewHolder.java
+++ app/src/main/java/com/tokenbrowser/view/adapter/viewholder/PaymentRequestViewHolder.java
@@ -1,50 +1,49 @@
 package com.tokenbrowser.view.adapter.viewholder;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.tokenbrowser.token.R;
+import com.bumptech.glide.Glide;
 import com.tokenbrowser.model.sofa.PaymentRequest;
+import com.tokenbrowser.token.R;
 import com.tokenbrowser.util.EthUtil;
 import com.tokenbrowser.view.BaseApplication;
 import com.tokenbrowser.view.adapter.listeners.OnItemClickListener;
 
 public final class PaymentRequestViewHolder extends RecyclerView.ViewHolder {
 
-    private View localView;
-    private View remoteView;
-    private TextView localRequestedAmount;
-    private TextView remoteRequestedAmount;
-    private TextView localSecondaryAmount;
-    private TextView remoteSecondaryAmount;
-    private ImageView remoteStatusIcon;
-    private TextView remoteStatus;
-    private View buttonsContainer;
-    private Button approveButton;
-    private Button rejectButton;
+    private @NonNull TextView requestedAmount;
+    private @NonNull TextView ethereumAmount;
+    private @Nullable ImageView statusIcon;
+    private @Nullable TextView statusMessage;
+    private @Nullable View acceptButtonContainer;
+    private @Nullable View declineButtonContainer;
+    private @Nullable Button acceptButton;
+    private @Nullable Button declineButton;
+    private @Nullable ImageView avatar;
 
     private OnItemClickListener<Integer> onApproveListener;
     private OnItemClickListener<Integer> onRejectListener;
 
     private PaymentRequest request;
-    private boolean sentByLocal;
+    private String avatarUri;
 
     public PaymentRequestViewHolder(final View v) {
         super(v);
-        this.localView = v.findViewById(R.id.local);
-        this.remoteView = v.findViewById(R.id.remote);
-        this.localRequestedAmount = (TextView) v.findViewById(R.id.local_requested_amount);
-        this.remoteRequestedAmount = (TextView) v.findViewById(R.id.remote_requested_amount);
-        this.localSecondaryAmount = (TextView) v.findViewById(R.id.local_eth_amount);
-        this.remoteSecondaryAmount = (TextView) v.findViewById(R.id.remote_eth_amount);
-        this.remoteStatusIcon = (ImageView) v.findViewById(R.id.request_status_icon);
-        this.remoteStatus = (TextView) v.findViewById(R.id.request_status);
-        this.buttonsContainer = v.findViewById(R.id.buttons_container);
-        this.approveButton = (Button) v.findViewById(R.id.approve_button);
-        this.rejectButton = (Button) v.findViewById(R.id.reject_button);
+        this.requestedAmount = (TextView) v.findViewById(R.id.requested_amount);
+        this.ethereumAmount = (TextView) v.findViewById(R.id.eth_amount);
+        this.statusIcon = (ImageView) v.findViewById(R.id.status_icon);
+        this.statusMessage = (TextView) v.findViewById(R.id.status_message);
+        this.acceptButtonContainer = v.findViewById(R.id.container_accept_button);
+        this.declineButtonContainer = v.findViewById(R.id.container_decline_button);
+        this.acceptButton = (Button) v.findViewById(R.id.approve_button);
+        this.declineButton = (Button) v.findViewById(R.id.reject_button);
+        this.avatar = (ImageView) v.findViewById(R.id.avatar);
     }
 
     public PaymentRequestViewHolder setPaymentRequest(final PaymentRequest request) {
@@ -52,8 +51,8 @@ public final class PaymentRequestViewHolder extends RecyclerView.ViewHolder {
         return this;
     }
 
-    public PaymentRequestViewHolder setSentByLocal(final boolean sentByLocal) {
-        this.sentByLocal = sentByLocal;
+    public PaymentRequestViewHolder setAvatarUri(final String uri) {
+        this.avatarUri = uri;
         return this;
     }
 
@@ -68,51 +67,60 @@ public final class PaymentRequestViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void draw() {
-        if (this.sentByLocal) {
-            this.localView.setVisibility(View.VISIBLE);
-            this.remoteView.setVisibility(View.GONE);
-            this.localRequestedAmount.setText(this.request.getLocalPrice());
-            final String ethAmount = String.format(
-                    BaseApplication.get().getResources().getString(R.string.eth_amount),
-                    EthUtil.hexAmountToUserVisibleString(this.request.getValue()));
-            this.localSecondaryAmount.setText(ethAmount);
-        } else {
-            this.remoteView.setVisibility(View.VISIBLE);
-            this.localView.setVisibility(View.GONE);
-            this.remoteRequestedAmount.setText(this.request.getLocalPrice());
-            final String ethAmount = String.format(
-                    BaseApplication.get().getResources().getString(R.string.eth_amount),
-                    EthUtil.hexAmountToUserVisibleString(this.request.getValue()));
-            this.remoteSecondaryAmount.setText(ethAmount);
-            setStatus(this.request.getState());
-        }
+        renderAmounts();
+        renderStatus();
+        renderAvatar();
     }
 
-    private void setStatus(final @PaymentRequest.State int state) {
+    private void renderAmounts() {
+        this.requestedAmount.setText(this.request.getLocalPrice());
+        final String ethAmount = String.format(
+                BaseApplication.get().getResources().getString(R.string.eth_amount),
+                EthUtil.hexAmountToUserVisibleString(this.request.getValue()));
+        this.ethereumAmount.setText(ethAmount);
+    }
+
+    private void renderStatus() {
+        if (this.statusMessage == null) {
+            return;
+        }
+
+        this.acceptButtonContainer.setVisibility(View.GONE);
+        this.declineButtonContainer.setVisibility(View.GONE);
+        this.statusMessage.setVisibility(View.VISIBLE);
+        this.statusIcon.setVisibility(View.VISIBLE);
+
+        final @PaymentRequest.State int state = this.request.getState();
         switch (state) {
             case PaymentRequest.ACCEPTED:
-                this.buttonsContainer.setVisibility(View.GONE);
-                this.remoteStatus.setVisibility(View.VISIBLE);
-                this.remoteStatus.setText(R.string.payment_request__accepted);
-                this.remoteStatusIcon.setVisibility(View.VISIBLE);
-                this.remoteStatusIcon.setImageResource(R.drawable.ic_done_with_background);
+                this.statusMessage.setText(R.string.payment_request__accepted);
+                this.statusIcon.setImageResource(R.drawable.ic_done_with_background);
                 break;
             case PaymentRequest.REJECTED:
-                this.buttonsContainer.setVisibility(View.GONE);
-                this.remoteStatus.setVisibility(View.VISIBLE);
-                this.remoteStatus.setText(R.string.payment_request__rejected);
-                this.remoteStatusIcon.setVisibility(View.VISIBLE);
-                this.remoteStatusIcon.setImageResource(R.drawable.ic_clear_with_background);
+                this.statusMessage.setText(R.string.payment_request__rejected);
+                this.statusIcon.setImageResource(R.drawable.ic_clear_with_background);
                 break;
             case PaymentRequest.PENDING:
             default:
-                this.buttonsContainer.setVisibility(View.VISIBLE);
-                this.remoteStatus.setVisibility(View.GONE);
-                this.remoteStatusIcon.setVisibility(View.GONE);
-                this.rejectButton.setOnClickListener(this.handleOnRejectPressed);
-                this.approveButton.setOnClickListener(this.handleOnApprovePressed);
+                this.acceptButtonContainer.setVisibility(View.VISIBLE);
+                this.declineButtonContainer.setVisibility(View.VISIBLE);
+                this.statusMessage.setVisibility(View.GONE);
+                this.statusIcon.setVisibility(View.GONE);
+                this.declineButton.setOnClickListener(this.handleOnRejectPressed);
+                this.acceptButton.setOnClickListener(this.handleOnApprovePressed);
                 break;
         }
+    }
+
+    private void renderAvatar() {
+        if (this.avatar == null) {
+            return;
+        }
+
+        Glide
+                .with(this.avatar.getContext())
+                .load(this.avatarUri)
+                .into(this.avatar);
     }
 
     private final View.OnClickListener handleOnApprovePressed = new View.OnClickListener() {
