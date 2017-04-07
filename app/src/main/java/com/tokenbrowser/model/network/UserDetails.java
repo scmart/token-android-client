2afc8cc45cf80bda8ff3e3f99580ae65814e68a0
csdodd <colin@bakkenbaeck.no>
2017-04-07 12:46:12 +0200

    Change format of data when registering/updating user

--- app/src/main/java/com/tokenbrowser/model/network/UserDetails.java
+++ app/src/main/java/com/tokenbrowser/model/network/UserDetails.java
@@ -14,7 +14,13 @@ public class UserDetails {
     private String payment_address;
 
     @JsonProperty
-    private CustomPayload custom;
+    private String about;
+
+    @JsonProperty
+    private String location;
+
+    @JsonProperty
+    private String name;
 
     public UserDetails setUsername(final String username) {
         this.username = username;
@@ -27,38 +33,17 @@ public class UserDetails {
     }
 
     public UserDetails setAbout(final String about) {
-        initCustom();
-        this.custom.about = about;
+        this.about = about;
         return this;
     }
 
     public UserDetails setLocation(final String location) {
-        initCustom();
-        this.custom.location = location;
+        this.location = location;
         return this;
     }
 
     public UserDetails setDisplayName(final String name) {
-        initCustom();
-        this.custom.name = name;
+        this.name = name;
         return this;
-    }
-
-    private void initCustom() {
-        if (custom == null) {
-            this.custom = new CustomPayload();
-        }
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private static class CustomPayload {
-        @JsonProperty
-        private String about;
-
-        @JsonProperty
-        private String location;
-
-        @JsonProperty
-        private String name;
     }
 }
