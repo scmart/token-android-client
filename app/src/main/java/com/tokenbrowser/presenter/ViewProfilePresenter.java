441b0496c38cbc9ed9aed4b22e08774bded4049f
Martin Tøften <martin@bakkenbaeck.no>
2017-04-10 11:52:37 +0200

    Show missing text after rating count

--- app/src/main/java/com/tokenbrowser/presenter/ViewProfilePresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/ViewProfilePresenter.java
@@ -136,10 +136,12 @@ public final class ViewProfilePresenter implements Presenter<ViewProfileActivity
 
     private void handleReputationResponse(final ReputationScore reputationScore) {
         if (this.activity == null) return;
-        final String reviewCount = String.valueOf(reputationScore.getCount());
+        final String ratingText = this.activity.getResources().getQuantityString(R.plurals.ratings, reputationScore.getCount());
+        final String rating = String.format("%s %s", reputationScore.getCount(), ratingText);
+        this.activity.getBinding().reviewCount.setText(rating);
+
         final double score = reputationScore.getScore() != null ? reputationScore.getScore() : 0;
         final String stringScore = String.valueOf(score);
-        this.activity.getBinding().reviewCount.setText(reviewCount);
         this.activity.getBinding().ratingView.setStars(score);
         this.activity.getBinding().reputationScore.setText(stringScore);
         this.activity.getBinding().ratingInfo.setRatingInfo(reputationScore);
--- app/src/main/java/com/tokenbrowser/presenter/ViewUserPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/ViewUserPresenter.java
@@ -106,10 +106,12 @@ public final class ViewUserPresenter implements Presenter<ViewUserActivity> {
     }
 
     private void handleReputationResponse(final ReputationScore reputationScore) {
-        final String reviewCount = String.valueOf(reputationScore.getCount());
+        final String ratingText = this.activity.getResources().getQuantityString(R.plurals.ratings, reputationScore.getCount());
+        final String rating = String.format("%s %s", reputationScore.getCount(), ratingText);
+        this.activity.getBinding().reviewCount.setText(rating);
+
         final double score = reputationScore.getScore() != null ? reputationScore.getScore() : 0;
         final String stringScore = String.valueOf(score);
-        this.activity.getBinding().reviewCount.setText(reviewCount);
         this.activity.getBinding().ratingView.setStars(score);
         this.activity.getBinding().reputationScore.setText(stringScore);
         this.activity.getBinding().ratingInfo.setRatingInfo(reputationScore);
