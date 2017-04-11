7868c3597bfdb28fe784005408e2f8b5b340de68
csdodd <colin@bakkenbaeck.no>
2017-04-11 09:34:17 +0200

    Bug: Has backedUpPhrase was selected after creating new account
    This change makes it only auto-selected after supplying a backup phrase and logging in.

--- app/src/main/java/com/tokenbrowser/presenter/SignInPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/SignInPresenter.java
@@ -71,6 +71,7 @@ public class SignInPresenter implements Presenter<SignInActivity> {
                         .getTokenManager()
                         .init(wallet))
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess(__ -> SharedPrefsUtil.setHasBackedUpPhrase())
                 .subscribe(
                         unused -> handleSuccess(),
                         throwable -> handleError());
@@ -94,8 +95,6 @@ public class SignInPresenter implements Presenter<SignInActivity> {
 
     private void goToMainActivity() {
         SharedPrefsUtil.setSignedIn();
-        // We just assume they have backed up their phrase since they just managed to log in with a phrase
-        SharedPrefsUtil.setHasBackedUpPhrase();
         final Intent intent = new Intent(this.activity, MainActivity.class);
         this.activity.startActivity(intent);
         this.activity.finish();
